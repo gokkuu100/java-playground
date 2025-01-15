c4fa5824d36481cc7fab12376e5f7f38a7e86f82
gokkuu100 <wkurts247@gmail.com>
2025-01-15 12:22:54 +0300

    threads

--- src/Second.java
+++ src/Second.java
@@ -1,27 +1,17 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-
-public class Second {
+public class Second extends Thread {
+    public static int amount = 0;
 
     public static void main(String[] args) {
-        // Make a collection
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(15);
-        list.add(8);
-        list.add(6);
-        list.add(11);
+        Second thread = new Second();
+        thread.start();
+        System.out.println(amount);
+        amount++;
+        System.out.println(amount);
+    }
 
-        // Get the iterator
-        Iterator<Integer> it = list.iterator();
-        while (it.hasNext()) {
-            Integer i = it.next();
-            if (i > 10) {
-                it.remove();
-            }
-        }
-
-        // Print the first item
-        System.out.println(list);
+    @Override
+    public void run() {
+        amount++;
     }
 }
 
