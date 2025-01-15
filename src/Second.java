ce088c254e15334f5a00be6f3a56c051abc07255
gokkuu100 <wkurts247@gmail.com>
2025-01-15 09:53:33 +0300

    hashmaps

--- src/Second.java
+++ src/Second.java
@@ -1,26 +1,12 @@
-interface FirstInterface {
-    public void myMethod();
-}
-interface SecondInterface {
-    public void myOtherMethod();
-}
+import java.util.HashMap;
 
-class Demo implements FirstInterface, SecondInterface {
-    @Override
-    public void myMethod() {
-        System.out.println("My method one");
-    }
-
-    @Override
-    public void myOtherMethod() {
-        System.out.println("My other method two");
-    }
-}
 public class Second {
+
     public static void main(String[] args) {
-        Demo myObj = new Demo();
-        myObj.myMethod();
-        myObj.myOtherMethod();
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        System.out.println(capitalCities);
     }
 }
 
