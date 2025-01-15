65a5745b43d52b6e7835661c0a0aa6a9f85042c3
gokkuu100 <wkurts247@gmail.com>
2025-01-15 10:37:57 +0300

    iterator

--- src/Second.java
+++ src/Second.java
@@ -1,12 +1,27 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Second {
 
     public static void main(String[] args) {
-        HashMap<String, String> capitalCities = new HashMap<String, String>();
-        capitalCities.put("England", "London");
-        capitalCities.put("Germany", "Berlin");
-        System.out.println(capitalCities);
+        // Make a collection
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(15);
+        list.add(8);
+        list.add(6);
+        list.add(11);
+
+        // Get the iterator
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            Integer i = it.next();
+            if (i > 10) {
+                it.remove();
+            }
+        }
+
+        // Print the first item
+        System.out.println(list);
     }
 }
 
