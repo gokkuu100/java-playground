c160bd80c56e1d47be36d05616e365a650cd4adb
gokkuu100 <wkurts247@gmail.com>
2025-01-16 20:46:16 +0300

    strings

--- src/Second.java
+++ src/Second.java
@@ -5,20 +5,53 @@ public class Second {
         double currentPopulation = p0;
 
         while (currentPopulation < p) {
-            currentPopulation += (currentPopulation * (percent / 100)) + aug;
+            currentPopulation += Math.floor((currentPopulation * (percent / 100)) + aug);
             years++;
         }
         return years;
     }
 
-    public static void main(String[] args) {
-        int p0 = 1500;
-        double percent = 5;
-        int aug = 100;
-        int p = 5000;
+    public void reverseString(char[] myArray) {
+        int left = 0;
+        int right = myArray.length -1;
+        while (left < right) {
+            char temp = myArray[left];
+            myArray[left] = myArray[right];
+            myArray[right] = temp;
 
-        int years = nbYear(p0, percent, aug, p);
-        System.out.println("Years needed: " + years);
+            left++;
+            right--;
+        }
+    }
+
+
+    public static String convertToInitials(String name) {
+        String[] parts = name.split(" ");
+        String initials = parts[0].substring(0,1).toUpperCase() + "." + parts[1].substring(0,1).toUpperCase();
+        return initials;
+    }
+
+    public static String fakeBin(String numberString) {
+        StringBuilder result = new StringBuilder();
+        for (char digit: numberString.toCharArray()) {
+            if (digit < '5') {
+                result.append('0');
+            } else {
+                result.append('1');
+            }
+        }
+        return result.toString();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(convertToInitials("Prince Hope"));
+        System.out.println(convertToInitials("Walter Hope"));
+
+        String input = "45385593107843568";
+        String output = fakeBin(input);
+        System.out.println("Input: " + input);
+        System.out.println("Output: " + output);
     }
 
 }
