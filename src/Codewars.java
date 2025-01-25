62b2a8069e9cc1a081fcc8e65fdfac56f482ae01
gokkuu100 <wkurts247@gmail.com>
2025-01-25 20:15:51 +0300

    codewars

--- src/Codewars.java
+++ src/Codewars.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.util.Arrays.stream;
 
 public class Codewars {
@@ -11,16 +14,27 @@ public class Codewars {
         return sorted[sorted.length-1];
     }
 
-    public static boolean feast(String beast, String dish) {
+    public static int howOld(final String herOld) {
+        String age = Character.toString(herOld.charAt(0));
+        return Integer.parseInt(age);
+    }
 
-        return false;
-
+    public static List<String> friend(List<String> x){
+        List<String> friends = new ArrayList<>();
+        for (String name: x) {
+            if (name.length()<=4) {
+                friends.add(name);
+            }
+        }
+        return friends;
     }
 
 
     public static void main(String[] args) {
         int[] myList = {8,8,6,4,7,3,2};
+        List<String> names = List.of("John", "Mark", "Amy", "Paul", "Elizabeth");
         System.out.println(min(myList));
         System.out.println(max(myList));
+        System.out.println(friend(names));
     }
 }
