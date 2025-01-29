6864f852ed36c096c48d34fc7db7099643e32e97
gokkuu100 <wkurts247@gmail.com>
2025-01-29 19:07:11 +0300

    getCount

--- src/Second.java
+++ src/Second.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.stream.StreamSupport;
 
 public class Second {
     public static int nbYear(int p0, double percent, int aug, int p) {
@@ -206,9 +205,20 @@ public class Second {
         return result.toString();
     }
 
+    public static int getCount(String str) {
+        int count = 0;
+        for (char c: str.toCharArray()) {
+            if ("aeiou".indexOf(c) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         System.out.println(century(1705));
         System.out.println(encode("recede"));
+        System.out.println(getCount("eld63rly"));
     }
 }
 
