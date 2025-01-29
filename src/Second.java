cbcc1e5f3fb5fe453cf4d6875807b41b5aa664e1
gokkuu100 <wkurts247@gmail.com>
2025-01-29 21:37:25 +0300

    sumsmallestnumbers

--- src/Second.java
+++ src/Second.java
@@ -205,6 +205,12 @@ public class Second {
         return result.toString();
     }
 
+    public static long sumTwoSmallestNumbers(long[] numbers) {
+        //Your solution here
+        Arrays.sort(numbers);
+        return numbers[0]+numbers[1];
+    }
+
     public static int getCount(String str) {
         int count = 0;
         for (char c: str.toCharArray()) {
