3858e5441ca9ef4a4344b2ceda6689ac8d398377
gokkuu100 <wkurts247@gmail.com>
2025-02-11 22:27:03 +0300

    twosums

--- src/Leetcode.java
+++ src/Leetcode.java
@@ -333,8 +333,25 @@ public class Leetcode {
         return result;
     }
 
+    public static int[] twoSums(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length;i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(count("aba"));
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(result);
     }
 }
